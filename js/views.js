03f5552633146ee3e1735b8a1df4f1a1a42de098
Sumeet Ranka <sumeet.ranka47@gmail.com>
2017-11-25 01:55:32 +0530

    about.html CV.html added; background added

--- js/views.js
+++ js/views.js
@@ -1,9 +1,28 @@
+menuToHTML = {
+    "home": "",
+    "about": "about",
+    "projects": "projects",
+    "CV": "CV",
+    "links": "links"
+}
+
 function load(that){
-$.ajax({
-    url: "./pages/about.html", 
-    context: document.body,
-    success: function(response) {
-        $("#details").html(response);
+    if(menuToHTML[that.innerText.trim()]){
+        $.ajax({
+            url: "./pages/" + menuToHTML[that.innerText.trim()] + ".html", 
+            context: document.body,
+            success: function(response) {
+                $("#details").html(response);
+            }
+        });   
     }
-});
+    else{
+        $.ajax({
+            url: ".", 
+            context: document.body,
+            success: function(response) {
+                $("html").html(response);
+            }
+        });     
+    }
 }
