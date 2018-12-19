a8ed1f86cdf2df9b17e0a100d98b0556e1c46935
Sumeet Ranka <sumeet.ranka47@gmail.com>
2018-12-19 00:08:22 -0500

    Revamp of the website; New content added

--- js/views.js
+++ js/views.js
@@ -1,52 +1,7 @@
-menuToHTML = {
-    "home": "",
-    "about": "about",
-    "projects": "projects",
-    "CV": "CV",
-    "links": "links",
-    "talks": "talks",
-    "contact": "contact"
-}
+/*!
+ * Start Bootstrap - Resume v5.0.2 (https://startbootstrap.com/template-overviews/resume)
+ * Copyright 2013-2018 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-resume/blob/master/LICENSE)
+ */
 
-function load(that){
-    if(menuToHTML[that.innerText.trim()]){
-        $.ajax({
-            url: "./pages/" + menuToHTML[that.innerText.trim()] + ".html", 
-            context: document.body,
-            success: function(response) {
-                $("#details").html(response);
-            }
-        });   
-    }
-    else{
-        $.ajax({
-            url: ".", 
-            context: document.body,
-            success: function(response) {
-                $("html").html(response);
-            }
-        });     
-    }
-    showProjectList(that.innerText.trim())
-}
-
-var myIndex = 0;
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    x[myIndex%x.length].style.display = "block";
-    x[(myIndex+1)%x.length].style.display = "block";  
-    setTimeout(carousel, 5000);
-    myIndex = (myIndex+1)%x.length;
-}
-
-function showProjectList(pageType){
-    if(pageType == "projects")
-        document.getElementById("project-list").style.visibility = "visible";
-    else
-        document.getElementById("project-list").style.visibility = "hidden";
-}
+!function(t){"use strict";t('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=t(this.hash);if((e=e.length?e:t("[name="+this.hash.slice(1)+"]")).length)return t("html, body").animate({scrollTop:e.offset().top},1e3,"easeInOutExpo"),!1}}),t(".js-scroll-trigger").click(function(){t(".navbar-collapse").collapse("hide")}),t("body").scrollspy({target:"#sideNav"})}(jQuery);
